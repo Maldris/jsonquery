230889a1195ed2d5f83f65cac4a687c14f45b814
Scott Owens <sdark.arrow@gmail.com>
2022-03-16 16:16:10 +1100

    ensuring a bool property gets parsed for test coverages and avoid panic's

--- node_test.go
+++ node_test.go
@@ -10,6 +10,7 @@ const (
 	testJSON = `{
 		"name":"John",
 		"age":30,
+		"motorist": true,
 		"cars": [
 			{ "name":"Ford", "models":[ "Fiesta", "Focus", "Mustang" ] },
 			{ "name":"BMW", "models":[ "320", "X3", "X5" ] },
